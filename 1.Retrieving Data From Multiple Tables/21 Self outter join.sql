25aabec584d43545bff9642e92e7609f4941f680
Ryan <douchangxu@gmail.com>
2026-08-12 18:25:21 +0200

    Delete 1.Retrieving Data From Multiple Tables directory

--- 1.Retrieving Data From Multiple Tables/21 Self outter join.sql
+++
@@ -1,8 +0,0 @@
-use sql_hr;
-SELECT 
-	e.employee_id,
-    e.first_name, 
-    m.first_name AS manager
-FROM employees e
-LEFT JOIN employees m
-	ON e.reports_to = m.employee_id;
--- 1.Retrieving Data From Multiple Tables/23.cross join.sql
+++
@@ -1,9 +0,0 @@
--- cross jion : create test data or create department table
-USE sql_store;
-SELECT * 
-FROM shippers, products;
-
--- explicit syntax
-select *
-FROM shippers
-CROSS JOIN products;
