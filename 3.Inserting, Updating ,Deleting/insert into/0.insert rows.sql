d9f9088027b092559cb9b00ca0b222a30d1bf207
Ryan <douchangxu@gmail.com>
2026-08-12 18:25:30 +0200

    Delete 3.Inserting, Updating ,Deleting directory

--- 3.Inserting, Updating ,Deleting/insert into/0.insert rows.sql
+++
@@ -1,6 +0,0 @@
--- insert 3 rows data to table products
-INSERT INTO products(product_id,name,quantity_in_stock,unit_price)
-VALUE(DEFAULT,'iphone11','10',4.1),
-	(DEFAULT,'iphone12','10',3.8),
-    (DEFAULT,'iphone14','10',2.7);
-    
