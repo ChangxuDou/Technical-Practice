d7976da313e46509dcf37f6a03de57b1c59dc6ce
Ryan <douchangxu@gmail.com>
2026-08-19 06:08:04 +0200

    Add files via upload

---
+++ SQL/6.Stored Procedures/2.Parameters.sql
@@ -0,0 +1,33 @@
+DROP PROCEDURE IF EXISTS get_client_by_state;
+
+DELIMITER $$
+CREATE PROCEDURE get_client_by_state
+(
+	state CHAR(2)
+)
+BEGIN
+
+SELECT * FROM clients c
+WHERE c.state = state;
+
+END $$
+DELIMITER  ;
+
+-- EXERCISE --
+-- Write a stored procedure to return invoices
+-- for a given client(client_id)
+-- Procedure name: get_invoices_by_client
+
+DROP PROCEDURE IF EXISTS get_invoice_by_client;
+
+DELIMITER $$
+CREATE PROCEDURE get_invoice_by_client
+(
+	client_id INT
+)
+BEGIN
+	SELECT * FROM invoices i
+    WHERE i.client_id = client_id;
+END $$
+
+DELIMITER ;
