b4277207503ada5be5d13d7c0364c14f15a53c02
Ryan <douchangxu@gmail.com>
2026-08-26 02:33:12 +0200

    Delete SQL/8.Triggers and Events directory

--- SQL/8.Triggers and Events/1.Viewing Triggers.sql
+++
@@ -1,5 +0,0 @@
-SHOW Triggers;
-SHOW Triggers LIKE 'payments%';
-SHOW CREATE TRIGGER payments_after_insert;
-
--- table_after_insert
--- SQL/8.Triggers and Events/4.VIewing Dropping and Altering Events.sql
+++
@@ -1,5 +0,0 @@
-SHOW EVENTS;
-SHOW EVENTS LIKE'yearly';
-DROP EVENT IF EXISTS yearly_delete_stale_audit_rows;
-ALTER EVENT... --like CREATE EVENT ,change Schedule directly
-ALTER EVENT yearly_delete_stale_audit_rows ENABLE;
