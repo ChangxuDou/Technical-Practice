cd5c71d5ebdfa8d82054d2723354b2452fbb6ac5
Ryan <douchangxu@gmail.com>
2026-08-26 02:30:34 +0200

    Delete SQL/0.Retrieving Data From a Single Table directory

--- SQL/0.Retrieving Data From a Single Table/10. NULL operator.sql
+++
@@ -1,5 +0,0 @@
--- get the orders that are not shipped
-
-SELECT * 
-FROM sql_store.orders
-WHERE shipped_date is NULL or shipper_id is NULL;
--- SQL/0.Retrieving Data From a Single Table/5.Between operator .sql
+++
@@ -1,8 +0,0 @@
--- select the customer 
--- 1000 <= points <= 3000
--- Between Operator
-
-SELECT * 
-FROM sql_store.customers
-where points between 1000 and 3000;
--- where points >= 1000 AND points <= 3000;
