d88f03a2999ee8f6f828b76f935df5fbb25abb14
Ryan <douchangxu@gmail.com>
2026-09-22 18:20:38 +0200

    Delete Python/10.Popular Python Packages/1.PyText- Text:Call:Whatsapp:Email directory

--- Python/10.Popular Python Packages/1.PyText- Text:Call:Whatsapp:Email/config.py
+++
@@ -1,7 +0,0 @@
-account_sid = "AC7ce8c81c8b8b4dc9ac99f721b86d5c19"
-auth_token = "8c0e188a21e82a54ca441a8a369c1989"
-my_tel = "+4915888620339"
-my_emi = "ryandouchangxu@gmail.com"
-
-sid = "SK2957974344cb430327659e6850e925cf"
-client_secret = "aV23uanXJvwQzoqelCdDLlw3KcZviQCt"
